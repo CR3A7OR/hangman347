cab1e750399ff2fd5e19aafa6fe48ccd8548856f
CR3A7OR <67477209+CR3A7OR@users.noreply.github.com>
2023-12-06 19:31:56 +0000

    Final version but not like final for AiCore

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 import random
 
+
+'''
+This class is used for the hangman game.
+
+    Attributes:
+        word: The word to be guessed, picked randomly from the word_list. Remember to import the random module into your script
+        word_guessed: list - A list of the letters of the word, with _ for each letter not yet guessed. For example, if the word is 'apple', the word_guessed list would be ['_', '_', '_', '_', '_']. If the player guesses 'a', the list would be ['a', '_', '_', '_', '_']
+        num_letters: int - The number of UNIQUE letters in the word that have not been guessed yet
+        num_lives: int - The number of lives the player has at the start of the game.
+        word_list: list - A list of words
+        list_of_guesses: list - A list of the guesses that have already been tried. Set this to an empty list initially
+
+'''
 class Hangman:
     def __init__(self,word_list,num_lives=5):
         self.num_lives = num_lives
@@ -11,6 +24,13 @@ class Hangman:
         self.list_of_guesses = []
     
     def check_guess(self,user_input):
+        '''
+        This function checks the user's guess
+
+        This will check if the user's guess is 
+        within the word chosen for hangman and 
+        handles the life counter
+        '''
         user_input = user_input.lower() 
         if user_input in self.word:
             print(f"Good guess! {user_input} is in the word")
@@ -24,7 +44,14 @@ class Hangman:
             self.num_lives -= 1
             print(f"You have {self.num_lives} lives left.")
 
-    def validate_input(self,user_input):
+    def __validate_input(self,user_input):
+        '''
+        This function validates the user input
+
+        This will take the user's input and check
+        for two things. If it is a single letter
+        and it hasn't already been guessed
+        '''
         if (len(user_input) == 1 and user_input.isalpha() and user_input not in self.list_of_guesses):
             print("Good Guess!")
             self.list_of_guesses.append(user_input)
@@ -36,21 +63,35 @@ class Hangman:
             return False
 
     def ask_for_input(self):
+        '''
+        This function asks for the user's input
+        '''
+        guess = input("Please enter a single letter: ")
+        if self.__validate_input(guess):   
+            self.check_guess(guess)
+        else:
+            pass
+    
+    def play_game(self):
+        '''
+        This function lets the user play the game
+        '''
         while True:
-            guess = input("Please enter a single letter: ")
-            if self.validate_input(guess):   
-                self.check_guess(guess)
+            if (self.num_lives > 0 and self.num_letters > 0):
+                self.ask_for_input()
+            elif(self.num_lives > 0 and self.num_letters == 0 ):
+                print("Won")
+                break
             else:
-                pass
-
-
+                print("You lose")
+                break
 
 
 def main():
     word_list = ['oranges', 'banana', 'apple', 'pear', 'peach']
-    num_lives = 3
-    player = Hangman(word_list,num_lives)
-    player.ask_for_input()
+    num_lives = 5
+    game = Hangman(word_list,num_lives)
+    game.play_game()
 
 if __name__ == "__main__":
     main()
